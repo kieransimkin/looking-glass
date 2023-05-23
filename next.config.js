428abfa6e39b7fcc63889cef09b0dfc70e186504
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2023-05-23 23:33:25 +0100

    weird wasm next.js bug workaround

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 const FilterWarningsPlugin = require('webpack-filter-warnings-plugin');
+
+const {join} = require('node:path');
+const {access, symlink} = require('node:fs/promises')
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
-  webpack5: false,
-  future: { 
-    webpack5: false
-  },
   webpack: function (config, options) {
-		config.experiments = { asyncWebAssembly: false, syncWebAssembly: true, layers: true,topLevelAwait: true };
+    const {isServer} = options;
+		config.experiments = { asyncWebAssembly: true, syncWebAssembly: true, layers: true,topLevelAwait: true };
     config.resolve.fallback = { fs: false, path: false };
-
+    
     // Trying to hide annoying warnings from serialization lib, none of this works, but leaving it here so I know what I've tried:
     config.stats={};
     config.stats.warningsFilter =[/emurgo/i]
@@ -20,6 +20,39 @@ const nextConfig = {
       })
     );
 
+
+    // There is some weird bug with dynamic wasm imports (again, triggered by serialization lib), this workaround is from github:
+    // https://github.com/vercel/next.js/issues/25852
+    config.plugins.push(
+      new (class {
+        apply(compiler) {
+          compiler.hooks.afterEmit.tapPromise(
+            'SymlinkWebpackPlugin',
+            async (compiler) => {
+              if (isServer) {
+                const from = join(compiler.options.output.path, '../static');
+                const to = join(compiler.options.output.path, 'static');
+    
+                try {
+                  await access(from);
+                  console.log(`${from} already exists`);
+                  return;
+                } catch (error) {
+                  if (error.code === 'ENOENT') {
+                    // No link exists
+                  } else {
+                    throw error;
+                  }
+                }
+    
+                await symlink(to, from, 'junction');
+                console.log(`created symlink ${from} -> ${to}`);
+              }
+            },
+          );
+        }
+      })(),
+    );
   // Important: return the modified config
     //return newconfig
 		return config;
