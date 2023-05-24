0d2c136556f022dfb8b958eed94ad23d700acf93
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2023-05-24 03:01:18 +0100

    oops forgot to commit this file

---
+++ components/NestedMenuItem.js
@@ -0,0 +1,178 @@
+import React, {useState, useRef, useImperativeHandle} from 'react'
+import {makeStyles} from '@material-ui/core/styles'
+import Menu, {MenuProps} from '@material-ui/core/Menu'
+import MenuItem, {MenuItemProps} from '@material-ui/core/MenuItem'
+import ArrowRight from '@material-ui/icons/ArrowRight'
+import ArrowLeft from '@material-ui/icons/ArrowLeft'
+import clsx from 'clsx'
+
+// This is copied from https://github.com/azmenak/material-ui-nested-menu-item
+// I added directionality, so that menus can open left as well as right. 
+// I've submitted a pull request to the original author:
+// https://github.com/azmenak/material-ui-nested-menu-item/pull/49
+
+const TRANSPARENT = 'rgba(0,0,0,0)'
+const useMenuItemStyles = makeStyles((theme) => ({
+  root: (props) => ({
+    backgroundColor: props.open ? theme.palette.action.hover : TRANSPARENT
+  })
+}))
+
+/**
+ * Use as a drop-in replacement for `<MenuItem>` when you need to add cascading
+ * menu elements as children to this component.
+ */
+const NestedMenuItem = React.forwardRef(function NestedMenuItem(props, ref) {
+  const {
+    parentMenuOpen,
+    component = 'div',
+    label,
+    direction = 'right',
+    rightIcon = <ArrowRight />,
+    leftIcon = <ArrowLeft />,
+    children,
+    className,
+    tabIndex: tabIndexProp,
+    MenuProps = {},
+    ContainerProps: ContainerPropsProp = {},
+    ...MenuItemProps
+  } = props
+
+  const {ref: containerRefProp, ...ContainerProps} = ContainerPropsProp
+
+  const menuItemRef = useRef();
+  
+  useImperativeHandle(ref, () => menuItemRef.current)
+  
+  
+
+  const containerRef = useRef();
+  useImperativeHandle(containerRefProp, () => containerRef.current)
+
+  const menuContainerRef = useRef();
+
+  const [isSubMenuOpen, setIsSubMenuOpen] = useState(false)
+
+  const handleMouseEnter = (event) => {
+    setIsSubMenuOpen(true)
+
+    if (ContainerProps?.onMouseEnter) {
+      ContainerProps.onMouseEnter(event)
+    }
+  }
+  const handleMouseLeave = (event) => {
+    setIsSubMenuOpen(false)
+
+    if (ContainerProps?.onMouseLeave) {
+      ContainerProps.onMouseLeave(event)
+    }
+  }
+
+  // Check if any immediate children are active
+  const isSubmenuFocused = () => {
+    const active = containerRef.current?.ownerDocument?.activeElement
+    for (const child of menuContainerRef.current?.children ?? []) {
+      if (child === active) {
+        return true
+      }
+    }
+    return false
+  }
+
+  const handleFocus = (event) => {
+    if (event.target === containerRef.current) {
+      setIsSubMenuOpen(true)
+    }
+
+    if (ContainerProps?.onFocus) {
+      ContainerProps.onFocus(event)
+    }
+  }
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Escape') {
+      return
+    }
+
+    if (isSubmenuFocused()) {
+      event.stopPropagation()
+    }
+
+    const active = containerRef.current?.ownerDocument?.activeElement
+
+    if (event.key === 'ArrowLeft' && isSubmenuFocused()) {
+      containerRef.current?.focus()
+    }
+
+    if (
+      event.key === 'ArrowRight' &&
+      event.target === containerRef.current &&
+      event.target === active
+    ) {
+      const firstChild = menuContainerRef.current?.children[0] 
+      firstChild?.focus()
+    }
+  }
+
+  const open = isSubMenuOpen && parentMenuOpen
+  const menuItemClasses = useMenuItemStyles({open})
+
+  // Root element must have a `tabIndex` attribute for keyboard navigation
+  let tabIndex
+  if (!props.disabled) {
+    tabIndex = tabIndexProp !== undefined ? tabIndexProp : -1
+  }
+  let iconStyle ={};
+  const icon = rightIcon;
+  if (direction=="left") { 
+    iconStyle={position:'absolute', left:'-4px', top:'5px'}
+    icon = leftIcon;
+  }
+  return (
+    <div
+      {...ContainerProps}
+      ref={containerRef}
+      onFocus={handleFocus}
+      tabIndex={tabIndex}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      onKeyDown={handleKeyDown}
+    >
+      <MenuItem
+        {...MenuItemProps}
+        className={clsx(menuItemClasses.root, className)}
+        ref={menuItemRef}
+      >
+        {label}
+      <div style={iconStyle}>{icon}</div>
+      </MenuItem>
+      <Menu
+        // Set pointer events to 'none' to prevent the invisible Popover div
+        // from capturing events for clicks and hovers
+        style={{pointerEvents: 'none'}}
+        anchorEl={menuItemRef?.current}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: direction=='right' ? 'right' : 'left'
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: direction=='right' ? 'left' : 'right'
+        }}
+        open={open}
+        autoFocus={false}
+        disableAutoFocus
+        disableEnforceFocus
+        onClose={() => {
+          setIsSubMenuOpen(false)
+        }}
+      >
+        <div ref={menuContainerRef} style={{pointerEvents: 'auto'}}>
+          {children}
+        </div>
+      </Menu>
+    </div>
+  )
+})
+
+export default NestedMenuItem
