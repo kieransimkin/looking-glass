2447e3e06d79da25e206e5fc1de0c20106b7ad04
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2023-05-28 06:33:00 +0100

    fix

--- pages/play.js
+++ pages/play.js
@@ -2,9 +2,10 @@ import Head from 'next/head'
 import Playground from '../components/Playground';
 
 export default function Play() {
-  let playgroundTag = <Playground loadStore />;
+  let playgroundTag = <Playground loadStored />;
   if (typeof localStorage != 'undefined' && localStorage.getItem('cip54-metadata')) { 
     playgroundTag = <Playground 
+                      loadStored
                       uses={JSON.parse(localStorage.getItem('cip54-features'))} 
                       metadata={JSON.parse(localStorage.getItem('cip54-metadata'))} 
                       programCode={localStorage.getItem('cip54-programCode')} 
