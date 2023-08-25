0b95894524fc9c85510f5b09e64ef05996babf9f
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2023-08-25 22:53:05 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -27,7 +27,7 @@ const nextConfig = {
   }
   //const paths = config.paths;
   config.output.path = path.resolve('build');
-  
+  config.entry= "build/pages/index.js";
   config.plugins.push( 
     new webpack.LoaderOptionsPlugin({
         test: /\.wasm$/,
@@ -36,7 +36,21 @@ const nextConfig = {
             type: 'webassembly/async',
         }
     }))
-    
+    config.plugins.push( 
+      new webpack.LoaderOptionsPlugin({
+          options: {
+              rules: [
+      // changed from { test: /\.jsx?$/, use: { loader: 'babel-loader' }, exclude: /node_modules/ },
+      { test: /\.(t|j)sx?$/, use: { loader: 'ts-loader' }, exclude: /node_modules/ },
+      { test: /\.(t|j)s?$/, use: { loader: 'ts-loader' }, exclude: /node_modules/ },
+      { test: /\.json?$/, use: { loader: 'ts-loader' }, exclude: /node_modules/ },
+
+      // addition - add source-map support
+      { enforce: "pre", test: /\.js$/, exclude: /node_modules/, loader: "source-map-loader" }
+              ]
+          }
+      
+      }));
     config.resolve.fallback = { fs: false, path: false };
     config.experiments = { asyncWebAssembly: true, layers: true, syncWebAssembly: true };
 		return config;
