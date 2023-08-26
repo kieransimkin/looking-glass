9bb292c4fd62751ffc53b26a49b8aa060e96d5f9
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2023-08-26 07:54:52 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -1,124 +1,85 @@
 /** @type {import('next').NextConfig} */
 const FilterWarningsPlugin = require('webpack-filter-warnings-plugin');
-const webpack = require('webpack');
-const path = require('path')
-const {access, symlink} = require('fs/promises');
-var {WebpackManifestPlugin }= require('webpack-manifest-plugin');
+
+const {join} = require('path');
+const {access, symlink} = require('fs/promises')
+
 const nextConfig = {
   reactStrictMode: false,
-  distDir:'./build/',
-  basePath:'/foo',
   swcMinify: true,
-  generateBuildId: async () => {
-    // Return custom build ID, like the latest git commit hash
-    return 'my-build-id'
-  },
   //output:'standalone',
-  webpack: function (config,  { env, paths, isServer }) {
-    config.optimization.moduleIds = 'named';  
-    config.name="foo"
-    config.context=path.resolve(__dirname, './build');
-    config.output={
-      path: path.resolve(__dirname, './build/'),
-      filename: '[name].[hash:8].js',
-      sourceMapFilename: '[name].[hash:8].map',
-      chunkFilename: '[id].[hash:8].js'
-    }
-    if (isServer) {
-      config.output.webassemblyModuleFilename = './../../static/wasm/[modulehash].wasm';
-  } else {
-      config.output.webassemblyModuleFilename = './static/wasm/[modulehash].wasm';
-  }
-  //const paths = config.paths;
-  //config.output.path = path.resolve('build');
-  const aliases = {'node_modules': path.resolve(__dirname, './node_modules'),'./node_modules': path.resolve(__dirname, './node_modules')};
-  aliases[path.resolve(__dirname,'./build/server')]=path.resolve(__dirname, './build');
-  
-  config.plugins.push(
-  new WebpackManifestPlugin ({
-    basePath: path.resolve(__dirname, './build'),
-    publicPath: '/assets/',appDirEnabled: false
-  }));
-  config.plugins.push( 
-  
-    new webpack.LoaderOptionsPlugin({
-        options: {
-          resolve: { 
-            'fallback':{fs: false, path: false},
-            'symlinks':true,
-            "alias":aliases
-          },
-          basePath:'/foo',
-          //context: path.resolve(__dirname, './build/'), 
-          name:"foo",
-          output:{
-            webassemblyModuleFilename: config.output.webassemblyModuleFilename,
-            path:path.resolve(__dirname, './build/'),
-            filename: '[name].[hash:8].js',
-            sourceMapFilename: '[name].[hash:8].map',
-            chunkFilename: '[id].[hash:8].js'
-          },
-            experiments: { 
-            asyncWebAssembly: true, syncWebAssembly: true, layers: true,topLevelAwait: true }}}));
-
-//config.entry= "pages/index.js";
-
-config.plugins.push(new webpack.IgnorePlugin({
-  resourceRegExp: /^pg$|^cloudflare:sockets$/,
-}))
-  //config.entry= 'pages/index.js'
-  config.plugins.push( 
-    new webpack.LoaderOptionsPlugin({
-        test: /\.wasm$/,
-        options: { 
-            
-            type: 'webassembly/async',
-        }
-    }))
-    /*
-    config.plugins.push( 
-      new webpack.LoaderOptionsPlugin({
-          options: {
-              rules: [
-      // changed from { test: /\.jsx?$/, use: { loader: 'babel-loader' }, exclude: /node_modules/ },
-      { test: /\.(t|j)sx?$/, use: { loader: 'next-swc-loader',  options: {
-        configFile: "tsconfig.json"
-    } }, exclude: /node_modules/ },
-      { test: /\.(t|j)s?$/, use: { loader: 'next-swc-loader',  options: {
-        configFile: "tsconfig.json"
-    } }, exclude: /node_modules/ },
-      { test: /\.json?$/, use: { loader: 'next-swc-loader',  options: {
-        configFile: "tsconfig.json"
-    } }, exclude: /node_modules/ },
-
-      // addition - add source-map support
-      
-      
-              ]
-          }
-      
-      }));
-    //*/
+  webpack: function (config, options) {
     
+		config.experiments = { asyncWebAssembly: true, syncWebAssembly: true, layers: true,topLevelAwait: true };
     config.resolve.fallback = { fs: false, path: false };
-    config.resolve.symlinks = true
-    const old = path.resolve(__dirname, './build/server')
-    config.resolve.alias={...config.resolve.alias, 
-      'node_modules': path.resolve(__dirname, './node_modules'),
-      './node_modules': path.resolve(__dirname, './node_modules')}
-      config.resolve.alias[old]=path.resolve(__dirname,'./build');
-    config.context = path.resolve(__dirname, './build')
-    config.experiments = { asyncWebAssembly: true,  layers: true,topLevelAwait: true,syncWebAssembly: true };
-    //config.entry= "pages/index.js";
-    console.log(config);
+
+    patchWasmModuleImport(config, options.isServer);
+    // Trying to hide annoying warnings from serialization lib, none of this works, but leaving it here so I know what I've tried:
+    config.stats={};
+    config.stats.warningsFilter =[/emurgo/i]
+    config.plugins.push(
+      new FilterWarningsPlugin({
+        exclude: /emurgo/
+      })
+    );
+
+    // There is some weird bug with dynamic wasm imports (again, triggered by serialization lib), this workaround is from github:
+    // https://github.com/vercel/next.js/issues/25852
+    /*
+    config.plugins.push(
+      new (class {
+        apply(compiler) {
+          compiler.hooks.afterEmit.tapPromise(
+            'SymlinkWebpackPlugin',
+            async (compiler) => {
+              if (isServer) {
+                const from = join(compiler.options.output.path, '../static');
+                const to = join(compiler.options.output.path, 'static');
+                
+                try {
+                  await access(from);
+                  console.log(`${from} already exists`);
+                  return;
+                } catch (error) {
+                  if (error.code === 'ENOENT') {
+                    // No link exists
+                  } else {
+                    throw error;
+                  }
+                }
+    
+                
+                await symlink(to, from, 'junction');
+                console.log(`created symlink ${from} -> ${to}`);
+              }
+            },
+          );
+        }
+      })(),
+    );
+    */
+  // Important: return the modified config
+    //return newconfig
 		return config;
 	},
-  experimental: { 
-      typedRoutes: false,
-      // scrollRestoration: true,
-    
-    forceSwcTransforms: false,
+}
+function patchWasmModuleImport(config, isServer) {
+  config.experiments = Object.assign(config.experiments || {}, {
+      asyncWebAssembly: true,
+  });
+
+  config.optimization.moduleIds = 'named';
+
+  config.module.rules.push({
+      test: /\.wasm$/,
+      type: 'webassembly/async',
+  });
+
+  // TODO: improve this function -> track https://github.com/vercel/next.js/issues/25852
+  if (isServer) {
+      config.output.webassemblyModuleFilename = './../static/wasm/[modulehash].wasm';
+  } else {
+      config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm';
   }
 }
-
 module.exports = nextConfig
