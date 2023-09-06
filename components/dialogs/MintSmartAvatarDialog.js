7fe0caaf772bb9a06fb6cee1de9695eb3ea9e069
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2023-09-06 05:26:34 +0100

    Update MintSmartAvatarDialog.js

--- components/dialogs/MintSmartAvatarDialog.js
+++ components/dialogs/MintSmartAvatarDialog.js
@@ -210,6 +210,7 @@ const Step1 = ({nextStep, onSpecChange, onFeatureTypeChange, goToStep, currentSt
   </>;
 };
 const Step2 = ({ spec, featureType, onSpecChange, previousStep, goToStep, nextStep, currentStep, handleClose, onImportChange }) => { 
+  const classes = useStyles();
   return <>
   <DialogContent className={classes.dialog}>
       <DialogTitle currentStep={currentStep} id="customized-dialog-title" onClose={handleClose} goToStep={goToStep}>
