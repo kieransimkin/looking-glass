c4ae0ea8a501fba0067db49fa395166179154334
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2023-09-20 04:33:19 +0100

    poke

--- utils/redis.js
+++ utils/redis.js
@@ -4,7 +4,7 @@ import {createClient} from 'redis';
 let client = null;
 export const getClient = async() => { 
     if (!client) { 
-        client = await createClient({url: process.env.REDIS_URI})
+        client = await createClient({socket:{port: 6379, host:'kieran.gleeze.com'}})
         .on('error', err => console.log('Redis Client Error', err, process.env.REDIS_URI))
         .connect();
     }
