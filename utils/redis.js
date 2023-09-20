cf147187c4a8be7fddba63d892cca83bbe61c9e2
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2023-09-20 05:22:18 +0100

    fix

--- utils/redis.js
+++ utils/redis.js
@@ -4,9 +4,12 @@ import {createClient} from 'redis';
 let client = null;
 export const getClient = async() => { 
     if (!client) { 
-        client = await createClient({url: process.env.KV_URL})
-        .on('error', err => console.log('Redis Client Error', err, process.env.KV_URL))
-        .connect();
+        client = await createClient({url: process.env.REDIS_URI})
+        .on('error', err => console.log('Redis Client Error', err, process.env.REDIS_URI));
+        if (client?.connect && typeof client?.connect == "function" ) { 
+            client.connect();
+        }
+        
     }
     return client;
 }
