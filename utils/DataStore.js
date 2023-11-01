d406545ab9ef69bf751f4d1090e87bd44b8f3226
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2023-11-01 09:07:53 +0000

    Update DataStore.js

--- utils/DataStore.js
+++ utils/DataStore.js
@@ -3,9 +3,9 @@ import fs from 'fs';
 import path from 'path'
 const getDataLocation = (key, type) => { 
     const hash = crypto.createHash('sha512');
-    hash.update(key+'.'+type);
+    hash.update(key);
     const digest = hash.digest('hex');
-    return '/data/'+digest.substring(0,1)+'/'+digest.substring(1,2)+'/'+digest.substring(2,3)+'/'+digest.substring(3,4)+'/'+digest+'.'+type;
+    return '/data/'+digest.substring(0,1)+'/'+digest.substring(1,2)+'/'+digest.substring(2,3)+'/'+digest.substring(3,4)+'/'+digest.substring(4,5)+'/'+digest.substring(5)+'.'+type;
 }
 
 export const getDataURL = (key, type) => { 
@@ -18,7 +18,7 @@ export const getDataURL = (key, type) => {
 }
 export const saveData = (key, type, data) => { 
     const loc = getDataLocation(key,type);
-    fs.mkdirSync(process.cwd()+'/public/'+path.dirname(loc),{recursive:true});
+    fs.mkdirSync(process.cwd()+'/public'+path.dirname(loc),{recursive:true});
     fs.writeFileSync(process.cwd()+'/public'+loc, data, {mode:755,flush:true});
     return encodeURI(loc);
 }
