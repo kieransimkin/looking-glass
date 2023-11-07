08c6a575ff219abff0d6368eccf479d63bc30d1e
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2023-11-07 08:11:14 +0000

    hmm not working

--- pages/policy/[...policy].js
+++ pages/policy/[...policy].js
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, useRef} from 'react';
 import { useRouter } from 'next/router';
 import dynamic from 'next/dynamic';
 import path from 'path';
@@ -7,6 +7,71 @@ import { useEffect } from 'react';
 import { getData, postData } from '../../utils/Api';
 import {MediaSlide} from 'react-mediaslide'
 import {SmartNFTPortal} from 'smartnftportal'
+import * as JsonTree from 'json-tree-viewer'
+import { useTheme } from '@material-ui/core';
+const renderFile= async (item, ready, width='100%', height='100%') => { 
+    let smI = {tokenUnit:''};
+    if (item.metadata?.uses) { 
+        const walletAddr = (await (await getData('/getTokenHolders?unit='+item.unit)).json())[0].stake;
+        const imports = await postData('/getSmartImports',{metadata: item.metadata, unit: item.unit, walletAddr});
+        const importJson = await imports.json();
+        smI=importJson;
+        
+    }
+    const doCallback = () => { 
+        ready();
+    }
+    return <SmartNFTPortal key={Math.random()} onReady={doCallback} loading={false} metadata={item.metadata} smartImports={smI} style={{width:width,height:height, borderWidth:'0', minWidth:'10px',minHeight:'10px'}} />
+}
+function BigInfoBox({item}) { 
+    const theme = useTheme();
+    const [ownerList, setOwnerList] = useState([]);
+    const [portalHTML, setPortalHTML] = useState(null);
+    const [portalOpacity, setPortalOpacity] = useState(0);
+    const metadataRef = useRef();
+    const imgRef = useRef();
+    const readyCallback = () => { 
+        setPortalOpacity(1);
+    }
+    useEffect(() => { 
+        const tree = JsonTree.create(item.metadata, metadataRef.current)
+        metadataRef.current.querySelectorAll('.jsontree_value_string').forEach((div)=>div.style.color=theme.palette.primary.main)
+        metadataRef.current.querySelectorAll('.jsontree_value_number').forEach((div)=>div.style.color='#ff0000')
+        metadataRef.current.querySelectorAll('.jsontree_value_boolean').forEach((div)=>div.style.color='#ff0000')
+        metadataRef.current.querySelectorAll('.jsontree_value_null').forEach((div)=>div.style.color='#ff0000')
+        setOwnerList([])
+        
+        setPortalOpacity(0);
+        renderFile(item,readyCallback, imgRef.current.clientWidth, imgRef.current.clientHeight).then((h) => { 
+            setPortalHTML(h);
+        })
+        
+        getData('/getTokenHolders?unit='+item.unit).then((data) => { 
+            data.json().then((o) => { 
+                
+                setOwnerList(o);
+                
+            })
+        })
+        return ()=> { 
+            if (metadataRef.current)  metadataRef.current.innerHTML='';
+        }
+    },[item])
+    
+    return <div style={{display:'flex', flexDirection:'column', alignItems: 'center', height:'100%'}}>
+    <div style={{position:'relative', marginTop: '1em', marginLeft: '1em'}}>
+        <img ref={imgRef} src={item.thumb} style={{maxWidth:'100%'}} />
+        <div style={{position: 'absolute', top: '0px', opacity:portalOpacity, transition: portalOpacity==1?'opacity 1s':""}}>
+        {portalHTML}
+        </div>
+        </div>
+        <h1>{item.title}</h1>
+        <ul>{ownerList.map((i) => <li>{i.stake}</li>)}</ul>
+        Metadata:
+        <div ref={metadataRef} style={{width:'100%', overflowX: 'auto', overflowY: 'auto'}} />
+        </div>
+        
+}
 export default  function CIP54Playground(params) {
     
     const router = useRouter();
@@ -26,21 +91,10 @@ export default  function CIP54Playground(params) {
         });
         
     },[policy])
-    const renderFile= async (item, ready, width='100%', height='100%') => { 
-        let smI = {tokenUnit:''};
-        if (item.metadata?.uses) { 
-            const walletAddr = (await (await getData('/getTokenHolders?unit='+item.unit)).json())[0].stake;
-            const imports = await postData('/getSmartImports',{metadata: item.metadata, unit: item.unit, walletAddr});
-            const importJson = await imports.json();
-            smI=importJson;
-            
-        }
-        const doCallback = () => { 
-            ready();
-        }
-        console.log(width,height);
-        return <SmartNFTPortal key={Math.random()} onReady={doCallback} loading={false} metadata={item.metadata} smartImports={smI} style={{width:width,height:height, borderWidth:'0', minWidth:'10px',minHeight:'10px'}} />
 
+    const renderBigInfo = async (i) => { 
+        
+        return <BigInfoBox item={i} />
     }
     const loadMoreData = ({page}) => { 
         setMediaSlideLoading(true);
@@ -62,7 +116,7 @@ export default  function CIP54Playground(params) {
     */
     return (
         <>
-            <MediaSlide renderFile={renderFile} onLoadMoreData={loadMoreData} loading={mediaSlideLoading} gallery={gallery?.tokens} pagination={{page: gallery?.page, totalPages: gallery?.totalPages }} />
+            <MediaSlide renderBigInfo={renderBigInfo} renderFile={renderFile} onLoadMoreData={loadMoreData} loading={mediaSlideLoading} gallery={gallery?.tokens} pagination={{page: gallery?.page, totalPages: gallery?.totalPages }} />
         </>
     );
 }
