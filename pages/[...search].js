98946e9167a52862d8841af3fa6f1065509f810d
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2023-11-09 04:46:59 +0000

    clean up

--- pages/[...search].js
+++ pages/[...search].js
@@ -42,27 +42,19 @@ export default function Search(params) {
     const router = useRouter();
     let {search} = router.query;  
     useEffect(() => { 
-        if (!search) return;
-        
-        console.log(search);
-        console.log(router);
+        if (!search) return;     
         if (validAddress(search[0])) { 
             router.push({pathname:'/wallet/'+search[0]})
         } else if (validatePolicyID(search[0]))  { 
             router.push({pathname:'/policy/'+search[0]})
         } else if (search[0].substring(0,1)=='$') { 
-            //const punycoded = punycode.encode(search[0].substr(1).trim()).trim();
             const punycoded = punycode.toASCII(search[0].substr(1).trim());
-            console.log(punycoded);
             getData('/getTokenHolders?unit=f0ff48bbb7bbe9d59a40f1ce90e9e9d0ff5002ec48f232b49ca0fb9a'+Buffer.from(punycoded).toString('hex')).then((a)=>{
                 a.json().then((j) => { 
                     if (j.length && j[0]?.address) { 
                         router.push({pathname:'/wallet/'+j[0].address})
                     }
-                    console.log(j);
                 });
-                
-
             });
         }
         
