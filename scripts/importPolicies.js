90f625dbf357b4862813ec4f4bd6bb778171e9e2
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2023-11-10 18:48:00 +0000

    adding import

--- scripts/importPolicies.js
+++ scripts/importPolicies.js
@@ -1,5 +1,10 @@
-async function doIt() { 
-const fileContents = await FileSystem.readFileSync(process.cwd()+'/data/policyIDs.json')
+const fs = require('fs');
+async function doIt() {
+const fileContents = await fs.readFileSync(process.cwd()+'/../data/policyIDs.json')
     console.log(fileContents);
+    const json = JSON.parse(fileContents)
+    for (var policy in json) { 
+        console.log(policy);
+    }
 }
 doIt();
