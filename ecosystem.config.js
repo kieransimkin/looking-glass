34286a18b237366345d8ee9fa1872abe76566e0d
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2023-11-10 15:10:22 +0000

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,9 @@
 module.exports = {
 	apps: [
 		{
-			script: 'npm run start',
+			script: 'node_modules/.bin/next',
 			instances: "max",
+            args: "start",
             "exec_mode": "cluster",
 			name: 'clg',
 		},
