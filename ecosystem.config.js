9055ea24e826a41045c4e7c85a2858eda747525b
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2023-11-10 15:19:49 +0000

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,3 +1,24 @@
+const path = require('path');
+const dotenv = require('dotenv');
+const fs = require('fs');
+function calcPath(relativePath) {
+    return path.join(__dirname, relativePath);
+  }
+  
+  // this function will parce `.env` file but not set them to `process.env`
+  const getEnvVariables = () => {
+    const envConfig = dotenv.parse(fs.readFileSync(calcPath('../clg.env')));
+  
+    const requiredEnvVariables = ['DATABASE_URI'];
+  
+    for (envVariable of requiredEnvVariables) {
+      if (!envConfig[envVariable]) {
+        throw new Error(`Environment variable "${envVariable}" is not set`);
+      }
+    }
+  
+    return envConfig;
+  };
 module.exports = {
 	apps: [
 		{
@@ -6,6 +27,7 @@ module.exports = {
             args: "start",
             "exec_mode": "cluster",
 			name: 'clg',
+            env: getEnvVariables()
 		},
 	],
 }
