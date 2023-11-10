7630d1a2ddbfb0593b5301edf8f0400bf906d0f1
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2023-11-10 19:31:59 +0000

    adding import

--- scripts/importPolicies.mjs
+++ scripts/importPolicies.mjs
@@ -4,12 +4,14 @@ import dotenv from 'dotenv';
 dotenv.config()
 async function doIt() {
 const fileContents = await fs.readFileSync(process.cwd()+'/data/policyIDs.json')
-    console.log(fileContents);
+    
     const json = JSON.parse(fileContents)
     for (var policy in json) { 
-        console.log(policy);
+        console.log('Doing policy: '+policy)
+        
         const existing = await database.getPolicy(policy);
         if (existing) {
+            console.log("Existing policy: "+existing.policyID)
             const result = await database.default.query(`
             UPDATE policy SET name=$1::TEXT, slug=$2::TEXT WHERE encode("policyID",'hex')=$3::TEXT
             `,[json[policy].title, json[policy].slug, existing.policyID])
