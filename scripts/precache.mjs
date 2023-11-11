3f61b659cb6c28ab1db27f15585282ded1e833e2
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2023-11-11 02:26:08 +0000

    Update precache.mjs

--- scripts/precache.mjs
+++ scripts/precache.mjs
@@ -1,19 +1,20 @@
 import fs from 'fs';
 import database from '../utils/database.js'
 import dotenv from 'dotenv';
+import * as formatter from '../utils/formatter';
 import redis from '../utils/redis.js'
 import syncClient from "../utils/dbsync.js";
 import libcip from "libcip54"
 dotenv.config()
 async function doIt() {
     const redisClient = await redis.getClient();
-    console.log(syncClient.default.query);
+    //console.log(syncClient.default.query);
     libcip.init('mainnet',syncClient.default, process.env.IPFS_GATEWAY, process.env.ARWEAVE_GATEWAY, redisClient)
     const results = await database.default.query(`
     select distinct "policyID", encode("policyID",'hex') as policy from policy
     `,[])
     for (var row of results.rows) { 
-        console.log(row);
+        
         const policy = row.policy;
         
         let tokens = await redis.checkCacheItem('getTokensFromPolicy:'+policy);
@@ -22,16 +23,22 @@ async function doIt() {
             await redis.cacheItem('getTokensFromPolicy:'+policy,tokens)
             console.log('Saving cache for row: '+policy)
         }
-        continue;
+    
+        
+        const page = 0;
+        const perPage = 10;
+        const start = page*perPage;
+        const end = (page+1)*perPage;
         const totalPages = Math.ceil(tokens.length/perPage);
         tokens = tokens.slice(start, end);
         
         const promises = [];
         for (const token of tokens) { 
-            promises.push(getTokenData(token));
+            promises.push(formatter.default.getTokenData(token));
         }
+        
         const result = await Promise.all(promises)
-        console.log(results);
+        console.log('Done '+policy)
     }
 
     return;
