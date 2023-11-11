6e18a5f4ae5466a6e781c7643ff39a741bd7dc71
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2023-11-11 00:15:49 +0000

    Update oura.js

--- pages/api/oura.js
+++ pages/api/oura.js
@@ -4,6 +4,6 @@ import pgClient from '../../utils/dbsync'
 export default async function Browse(req, res) {
     const redisClient = await getClient();
     init(process.env.NETWORK?.toLowerCase(), pgClient, process.env.IPFS_GATEWAY, process.env.ARWEAVE_GATEWAY, redisClient);
-    console.log(req.body);
-
+    console.error(req.body);
+    res.status(200).json({ok:'ok'});
 }
