8db76f9e8d022dd61bf5a52c51bb5a2619cb2a6f
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2023-11-11 21:39:38 +0000

    I dont understand these stupid import issues

--- scripts/refreshWallet.mjs
+++ scripts/refreshWallet.mjs
@@ -5,13 +5,13 @@ import * as formatter from '../utils/formatter.js';
 import redis from '../utils/redis.js'
 
 import syncClient from "../utils/dbsync.js";
-import libcip from "libcip54"
+import * as libcip from "libcip54"
 import * as helpers from '../utils/Helpers.js';
 dotenv.config()
 async function doIt() {
     const redisClient = await redis.getClient();
     //console.log(syncClient.default.query);
-    libcip.init('mainnet',syncClient.default, process.env.IPFS_GATEWAY, process.env.ARWEAVE_GATEWAY, redisClient)
+    libcip.default.init('mainnet',syncClient.default, process.env.IPFS_GATEWAY, process.env.ARWEAVE_GATEWAY, redisClient)
     const results = await database.default.query(`
     select distinct "policyID", encode("policyID",'hex') as policy from policy
     `,[])
@@ -22,7 +22,9 @@ async function doIt() {
         if (item.timestamp<(Date.now()-600000)) { 
             let tokens = await redis.checkCacheItem('getTokensFromAddress:'+stake);
             if (!tokens) {   
-                tokens = await libcip.getTokensFromAny(stake);
+                libcip.default.getTokensFromAny
+                tokens = await libcip.default.getTokensFromAny(stake);
+                
                 console.log('Cached tokens for '+stake)
                 await redis.cacheItem('getTokensFromAddress:'+stake,tokens)
             }
