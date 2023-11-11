a2e0e96f1bb4ce4537e39500210319efcea043fd
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2023-11-11 22:18:32 +0000

    fix

--- scripts/refreshWallet.mjs
+++ scripts/refreshWallet.mjs
@@ -11,7 +11,7 @@ dotenv.config()
 async function doIt() {
     const redisClient = await redis.getClient();
     //console.log(syncClient.default.query);
-    libcip.default.init('mainnet',syncClient.default, process.env.IPFS_GATEWAY, process.env.ARWEAVE_GATEWAY, redisClient)
+    libcip.init('mainnet',syncClient.default, process.env.IPFS_GATEWAY, process.env.ARWEAVE_GATEWAY, redisClient)
     const results = await database.default.query(`
     select distinct "policyID", encode("policyID",'hex') as policy from policy
     `,[])
@@ -22,8 +22,8 @@ async function doIt() {
         if (item.timestamp<(Date.now()-600000)) { 
             let tokens = await redis.checkCacheItem('getTokensFromAddress:'+stake);
             if (!tokens) {   
-                libcip.default.getTokensFromAny
-                tokens = await libcip.default.getTokensFromAny(stake);
+                
+                tokens = await libcip.getTokensFromAny(stake);
                 
                 console.log('Cached tokens for '+stake)
                 await redis.cacheItem('getTokensFromAddress:'+stake,tokens)
