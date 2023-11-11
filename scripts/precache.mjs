85acc045da04b6413135283894e0dd60bda1e6ed
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2023-11-11 13:29:37 +0000

    Update precache.mjs

--- scripts/precache.mjs
+++ scripts/precache.mjs
@@ -20,7 +20,7 @@ async function doIt() {
         let tokens = await redis.checkCacheItem('getTokensFromPolicy:'+policy);
         if (!tokens) {
             tokens = await libcip.getTokensFromPolicy(policy);
-            await database.setPolicyAssetCount(policy, count(tokens))
+            await database.setPolicyAssetCount(policy, tokens.length)
             await redis.cacheItem('getTokensFromPolicy:'+policy,tokens)
             console.log('Saving cache for row: '+policy)
         }
