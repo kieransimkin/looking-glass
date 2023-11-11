58e66d21a84fd1ae1eefaf98a53a2062bf876fb6
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2023-11-11 05:32:15 +0000

    Update policy.js

--- models/policy.js
+++ models/policy.js
@@ -3,13 +3,13 @@ export async function setLastMinted(date) {
     
         await db.query(
         `
-        UPDATE policy SET "lastMinted"=$1::timestamp WHERE "policyID"=$2::TEXT
+        UPDATE policy SET "lastMinted"=$1::timestamp WHERE encode("policyID",'hex')=$2::TEXT
         `,[date, this.policyID]);
     
 }
 export async function setLastMoved(date) { 
     await db.query(
         `
-        UPDATE policy SET "lastMoved"=$1::timestamp WHERE "policyID"=$2::TEXT
+        UPDATE policy SET "lastMoved"=$1::timestamp WHERE encode("policyID",'hex')=$2::TEXT
         `,[date, this.policyID]);
 }
