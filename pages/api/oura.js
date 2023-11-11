032fb202add0665646d97b8e7e0d526fb01ea85e
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2023-11-11 00:10:51 +0000

    Update oura.js

--- pages/api/oura.js
+++ pages/api/oura.js
@@ -1,6 +1,6 @@
 import { getClient } from "../../utils/redis";
 import { init } from "libcip54";
-
+import pgClient from '../../utils/dbsync'
 export default async function Browse(req, res) {
     const redisClient = await getClient();
     init(process.env.NETWORK?.toLowerCase(), pgClient, process.env.IPFS_GATEWAY, process.env.ARWEAVE_GATEWAY, redisClient);
