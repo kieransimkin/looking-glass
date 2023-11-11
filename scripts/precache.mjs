d678e05303ab29824e7b7301193e238b6ba62e26
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2023-11-11 23:21:30 +0000

    fix

--- scripts/precache.mjs
+++ scripts/precache.mjs
@@ -46,7 +46,7 @@ async function doIt() {
             console.log('Page '+page+' of '+totalPages)
             await doPage(tokens, page);
             page++;
-            if (page>10) break;
+            
             //sleep(2000);
         }
         
@@ -58,24 +58,7 @@ async function doIt() {
     
     return;
     const json = JSON.parse(fileContents)
-    for (var policy in json) { 
-        console.log('Doing policy: '+policy)
-        
-        const existing = await database.getPolicy(policy);
-        if (existing) {
-            console.log("Existing policy: "+existing.policyID)
-            try { 
-            await database.default.query(`
-            UPDATE policy SET name=$1::TEXT, slug=$2::TEXT WHERE encode("policyID",'hex')=$3::TEXT
-            `,[json[policy].title, json[policy].slug, existing.policyID])
-            } catch (e) { 
-                console.log(e)
-            }
-            
-            console.log('found');
-        } else {
-            console.log('not found');
-        }
-    }
+    
 }
 doIt();
+
