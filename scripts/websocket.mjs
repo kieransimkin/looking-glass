3c97d67fc3f2c7b5b6b1c025df7194200c084063
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2023-11-17 01:18:15 +0000

    fix

--- scripts/websocket.mjs
+++ scripts/websocket.mjs
@@ -1,7 +1,7 @@
 import http from 'http';
 import { Server } from "socket.io";
 import cors from 'cors';
-
+import * as websocket from '../utils/websocket'
 const httpServer = http.createServer();
 
 const io = new Server(httpServer, {
@@ -10,24 +10,7 @@ const io = new Server(httpServer, {
     }
 });
 
-io.on("connection", (socket) => {
-  console.log("A user connected:", socket.id);
-  socket.on("join_room", (roomId) => {
-    socket.join(roomId);
-    console.log(`user with id-${socket.id} joined room - ${roomId}`);
-  });
-
-  socket.on("send_msg", (data) => {
-    console.log(data, "DATA");
-    //This will send a message to a specific room ID
-    socket.to(data.roomId).emit("receive_msg", data);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("A user disconnected:", socket.id);
-  });
-});
-
+io.on("connection", websocket.default.onConnection);
 const PORT = process.env.SOCKET_PORT || 3001;
 httpServer.listen(PORT, () => {
   console.log(`Socket.io server is running on port ${PORT}`);
