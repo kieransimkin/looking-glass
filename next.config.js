165b1f463d786baf39723f081524c98df49eb793
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2023-11-22 04:19:35 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -10,15 +10,6 @@ const nextConfig = {
   env: { 
     'SOCKET_PORT':process.env.SOCKET_PORT
   },
-  rewrites: () => { return[
-    
-      // we need to define a no-op rewrite to trigger checking
-      // all pages/static files before we attempt proxying
-      {
-        source: '/:path*',
-        destination: '/:path*',
-      }
-    ];},
   //output:'standalone',
   webpack: function (config, options) {
     
