38935f7606580a010664fa4fa3e6c11e23c83679
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2023-11-22 15:45:17 +0000

    fix

--- next.config.js
+++ next.config.js
@@ -1,29 +1,29 @@
 /** @type {import('next').NextConfig} */
 const FilterWarningsPlugin = require('webpack-filter-warnings-plugin');
-const withTM = require('next-transpile-modules')(['three'])
-const {join} = require('path');
-const {access, symlink} = require('fs/promises')
-const CopyPlugin = require("copy-webpack-plugin");
+const withTM = require('next-transpile-modules')(['three']);
+const { join } = require('path');
+const { access, symlink } = require('fs/promises');
+const CopyPlugin = require('copy-webpack-plugin');
 const nextConfig = {
   reactStrictMode: false,
-  swcMinify:false,
-  env: { 
-    'SOCKET_PORT':process.env.SOCKET_PORT
+  swcMinify: false,
+  env: {
+    SOCKET_PORT: process.env.SOCKET_PORT,
   },
-  rewrites: () => { return[
-    
+  rewrites: () => {
+    return [
       // we need to define a no-op rewrite to trigger checking
       // all pages/static files before we attempt proxying
       {
         source: '/:path*',
         destination: '/:path*',
-      }
-    ];},
+      },
+    ];
+  },
   webpack: function (config, options) {
-    
-		config.experiments = { asyncWebAssembly: true, syncWebAssembly: true, layers: true,topLevelAwait: true };
+    config.experiments = { asyncWebAssembly: true, syncWebAssembly: true, layers: true, topLevelAwait: true };
     config.resolve.fallback = { fs: false, path: false, dns: false, net: false, tls: false };
-/*
+    /*
     if (!options.dev && options.isServer) { 
   config.optimization.moduleIds = 'named';
 	    /*
@@ -37,8 +37,8 @@ const nextConfig = {
 	console.log(config.module.rules);
 	  console.log(config.output)
     // Trying to hide annoying warnings from serialization lib, none of this works, but leaving it here so I know what I've tried:
-   //*/ 
-	  /*
+   //*/
+    /*
 	  config.plugins.push( new CopyPlugin({
           patterns: [
               { from: "node_modules/@emurgo/cardano-serialization-lib-nodejs/cardano_serialization_lib_bg.wasm",
@@ -46,62 +46,60 @@ const nextConfig = {
           ],
       }))
       */
-      patchWasmModuleImport(config, options.isServer);
-    config.stats={};
-    config.stats.warningsFilter =[/emurgo/i]
+    patchWasmModuleImport(config, options.isServer);
+    config.stats = {};
+    config.stats.warningsFilter = [/emurgo/i];
     config.plugins.push(
       new FilterWarningsPlugin({
-        exclude: /emurgo/
-      })
+        exclude: /emurgo/,
+      }),
     );
 
-		return config;
-	},
-}
+    return config;
+  },
+};
 function patchWasmModuleImport(config, isServer) {
   config.experiments = Object.assign(config.experiments || {}, {
-      asyncWebAssembly: true,
+    asyncWebAssembly: true,
   });
-config.module.defaultRules = [ {
-                type: 'javascript/auto',
-                resolve: {},
-            },
-            {
-                test: /\.json$/i,
-                type: 'json',
-            },
-        ];
+  config.module.defaultRules = [
+    {
+      type: 'javascript/auto',
+      resolve: {},
+    },
+    {
+      test: /\.json$/i,
+      type: 'json',
+    },
+  ];
   config.optimization.moduleIds = 'named';
 
   config.module.rules.push({
-      test: /\.wasm$/,
-	  type: 'asset/resource'
+    test: /\.wasm$/,
+    type: 'asset/resource',
   });
 
   // TODO: improve this function -> track https://github.com/vercel/next.js/issues/25852
   if (isServer) {
-      config.output.webassemblyModuleFilename = './../static/wasm/[modulehash].wasm';
+    config.output.webassemblyModuleFilename = './../static/wasm/[modulehash].wasm';
   } else {
-      config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm';
+    config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm';
   }
 }
 class WasmChunksFixPlugin {
   apply(compiler) {
-    compiler.hooks.thisCompilation.tap("WasmChunksFixPlugin", (compilation) => {
-      compilation.hooks.processAssets.tap(
-        { name: "WasmChunksFixPlugin" },
-        (assets) =>
-          Object.entries(assets).forEach(([pathname, source]) => {
-            if (!pathname.match(/\.wasm$/)) return;
-            compilation.deleteAsset(pathname);
+    compiler.hooks.thisCompilation.tap('WasmChunksFixPlugin', (compilation) => {
+      compilation.hooks.processAssets.tap({ name: 'WasmChunksFixPlugin' }, (assets) =>
+        Object.entries(assets).forEach(([pathname, source]) => {
+          if (!pathname.match(/\.wasm$/)) return;
+          compilation.deleteAsset(pathname);
 
-            const name = pathname.split("/")[1];
-            const info = compilation.assetsInfo.get(pathname);
-            compilation.emitAsset(name, source, info);
-          })
+          const name = pathname.split('/')[1];
+          const info = compilation.assetsInfo.get(pathname);
+          compilation.emitAsset(name, source, info);
+        }),
       );
     });
   }
 }
-module.exports = withTM(nextConfig)
-
+module.exports = withTM(nextConfig);
