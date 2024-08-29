ce1a227668a9476ddb0eb4d59fec1c1c670cbfe5
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2024-08-29 01:37:16 +0100

    should've tested that

--- components/Layout.js
+++ components/Layout.js
@@ -99,7 +99,7 @@ const Layout = ({children}) => {
     }
     let thisTheme = themeState==='light' ? lightTheme : darkTheme;
     return (
-        <div id={state} style={{height: '100vh', fontFamily: "'MuseoModerno', cursive", backgroundColor:thisTheme.palette.background.default, color: thisTheme.palette.text.primary}}>
+        <div id={themeState} style={{height: '100vh', fontFamily: "'MuseoModerno', cursive", backgroundColor:thisTheme.palette.background.default, color: thisTheme.palette.text.primary}}>
         <ThemeProvider theme={thisTheme}>
             
                     <Header onThemeChange={onThemeChange} onWalletChange={onWalletChange} />
