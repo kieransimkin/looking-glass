bd829230a713471c333fed886542a552c771b0c7
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2024-09-09 12:51:42 +0100

    revert this

--- pages/policy/[...policy].js
+++ pages/policy/[...policy].js
@@ -194,7 +194,7 @@ export default  function CIP54Playground(props) {
         setBigInfoOpen(newBigInfoOpen);
         console.log('Called render biginfo');
         return <BigInfoBox onClose={onClose} goFullscreen={goFullscreen(i)} item={i} navbarHeight={navbarHeight} bigInfoOpen={newBigInfoOpen} />
-    },[true]);
+    },[bigInfoOpen, currentSelection]);
 
     useEffect(()=> { 
         const msgHandler = (e) => { 
